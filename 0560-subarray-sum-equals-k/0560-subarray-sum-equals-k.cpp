4c25632efc903d827f8bde20c047ca47047f0382
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-05-01 13:53:02 +0530

    Time: 40 ms (64.77%), Space: 45.4 MB (55.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,18 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         //Submitted for 100DaysLeetCodeChallenge DAY 51
 
-        vector <int> pre;
-        int sum = 0, ans = 0;
-        for(auto i : nums)
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int total = 0, ans = 0;
+
+        for (int n : nums) 
         {
-            sum+=i;
-            pre.push_back(sum);
+            total += n;
+            if (mp.find(total - k) != mp.end()) ans += mp[total - k];
+            mp[total]++;
         }
-        for(int i=0;i<pre.size();i++)
-        {
-            if(pre[i]==k) ans++;
-            for(int j=i+1;j<pre.size();j++) if(pre[j]-pre[i]==k) ans++;
-        }
+
         return ans;
     }
 };
