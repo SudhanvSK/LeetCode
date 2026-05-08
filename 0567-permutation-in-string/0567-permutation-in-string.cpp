891b78ec2b8d84ddca3ae7eb66b44cdbd447e22b
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-05-08 20:09:39 +0530

    Time: 7 ms (39.96%), Space: 10.1 MB (44.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
+    // Submitted for 100DaysLeetCodeChallenge DAY 58
+
     bool checkInclusion(string s1, string s2) {
-        // Submitted for 100DaysLeetCodeChallenge DAY 58
-        if(s1.size()>s2.size()) return false;
-        unordered_map <char, int> m1;
-        unordered_map <char, int> m2;
-        for(auto i:s1) m1[i]++;
-        for(int i=0;i<=s2.size()-s1.size();i++)
-        {
-            m2.clear();
-            bool ok = true;
-            for(int j=i;j<i+s1.size();j++) m2[s2[j]]++;
-            for(auto i:m1) if(i.second!=m2[i.first]) {ok=false;break;}
-            if(ok) return true;
+      int n1=s1.size(),n2=s2.size();
+      if(n1>n2) return false;
+      vector<int>freq1(26,0),freq2(26,0);
+      for(char c:s1) freq1[c-'a']++;
+      int left=0;
+      for(int right=0;right<n2;right++){
+        freq2[s2[right]-'a']++;
+        if(right-left+1>n1){
+            freq2[s2[left]-'a']--;
+            left++;
         }
-        return false;
+        if(freq1==freq2) return true;
+      }
+      return false;
     }
 };
