8ff887a7d0c9fde1ce4e7067a41aaf6b2a6b616b
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-05-25 10:30:15 +0530

    Time: 6 ms (62.62%), Space: 38.2 MB (94.61%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -3,10 +3,10 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
         // Submitted for 100DaysLeetCodeChallenge DAY 75
         unordered_map <char, int> mp;
-        int c = 0;
-        for(auto i:tasks) {mp[i]++; c = max(c,mp[i]);}
-        int ans = (c-1)*(n+1);
-        for(auto i:mp) if(i.second == c) ans++;
+        int cnt = 0;
+        for(auto i:tasks) {mp[i]++; cnt = max(cnt,mp[i]);}
+        int ans = (cnt-1)*(n+1);
+        for(auto i:mp) if(i.second == cnt) ans++;
         return max((int)tasks.size(),ans);
     }
 };
