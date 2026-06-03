e5a3f699cbba32b750d86931c6fd3511dd9cabc4
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-06-03 19:23:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,8 +2,17 @@ class Solution {
 public:
     int fib(int n) {
         // Submitted for 100DaysLeetCodeChallenge DAY 84
-        if(n==0) return 0;
-        if(n==1||n==2) return 1;
-        return fib(n-1) + fib(n-2);
+        int f1=0;
+        int f2=1;
+        if (n==0) return f1;
+        if (n==1) return f2;
+        for (int i=2;i<=n;i++)
+        {
+            int f3 = f2 + f1; 
+            f1 = f2;
+            f2 = f3; 
+        
+        }
+        return f2;
     }
 };
