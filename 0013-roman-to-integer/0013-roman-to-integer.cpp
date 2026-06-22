00d9778651bb6c8714c62a536c9d2f2cfe2d67ca
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-06-22 21:27:43 +0530

    Time: 4 ms (67.08%), Space: 13.2 MB (51.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,8 +5,8 @@ public:
             {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
         };
         int n = s.size();
-        int ans = mp[s[n-1]];
-        for(int i=n-2;i>=0;i--) if(mp[s[i]]<mp[s[i+1]]) ans-=mp[s[i]]; else ans+=mp[s[i]];
-        return ans;
+        int c = mp[s[n-1]];
+        for(int i=n-2;i>=0;i--) if(mp[s[i]]<mp[s[i+1]]) c-=mp[s[i]]; else c+=mp[s[i]];
+        return c;
     }
 };
