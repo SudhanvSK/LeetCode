04845ce0542e51c482cf6868b4299a7110b05472
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-06-24 23:06:32 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.28%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for(int i=s.size()-1;i>=0;i--) 
+        {
+            if(ans > 0 && s[i] == ' ') break;
+            if(s[i] != ' ') ans++;
+        }
+        return ans;
+    }
+};
