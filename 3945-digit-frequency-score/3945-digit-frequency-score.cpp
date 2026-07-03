046d7b18bb81bad34cda9b540c509f6738b9c0e5
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-07-03 20:06:17 +0530

    Time: 3 ms (17.59%), Space: 8.5 MB (95.25%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        unordered_map <int,int> mp;
+        int ans = 0;
         while(n>0)
         {
-            mp[n%10]++;
+            ans+=(n%10);
             n/=10;
         }
-        int ans = 0;
-        for(auto i : mp) ans+=(i.first*i.second);
         return ans;
     }
 };
